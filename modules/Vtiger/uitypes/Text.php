7ac31cf1633d1c422e21b044ae8279c33c4d5afb
chinseika <chin@thinkingreed.co.jp>
2026-08-07 18:52:25 +0900

    [追加] #1773 テキスト項目内のURLを自動リンク表示

--- modules/Vtiger/uitypes/Text.php
+++ modules/Vtiger/uitypes/Text.php
@@ -21,8 +21,28 @@ class Vtiger_Text_UIType extends Vtiger_Base_UIType {
 		}
                 if($removeTags){
                     $value = strip_tags($value,'<br>');
+                    return nl2br(purifyHtmlEventAttributes($value, true));
                 }
-		return nl2br(purifyHtmlEventAttributes($value, true));
+		return self::linkifyUrls(nl2br(purifyHtmlEventAttributes($value, true)));
+	}
+
+	/**
+	 * テキスト中のURLをリンク表示に変換する（既存タグ内は対象外、全角文字・閉じ括弧はURLに含めない） #1773
+	 * @param <String> $value
+	 * @return <String>
+	 */
+	public static function linkifyUrls($value) {
+		$pattern = '/<[^>]*>|(https?:\/\/[^\s<>"\'\x{3000}-\x{30FF}\x{4E00}-\x{9FFF}\x{FF00}-\x{FFEF})\]]+)/u';
+		$result = preg_replace_callback($pattern, function($matches) {
+			if(empty($matches[1])) {
+				return $matches[0];
+			}
+			$url = rtrim($matches[1], '.,');
+			$trail = substr($matches[1], strlen($url));
+			return '<a class="urlField cursorPointer" href="'.$url.'" target="_blank">'.$url.'</a>'.$trail;
+		}, $value);
+		// 不正なUTF-8バイト列を含む値ではnullが返るため、元の値をそのまま表示する
+		return $result === null ? $value : $result;
 	}
     
     /**
